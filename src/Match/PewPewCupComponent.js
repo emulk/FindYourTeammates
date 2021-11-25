2334b1b07fe54bac8af855498780405ade1d5237
Orgest Shehaj <Orgest.Shehaj@gidi.it>
2021-11-25 17:57:44 +0100

    season 2 page

--- src/Match/PewPewCupComponent.js
+++ src/Match/PewPewCupComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
-import { Nav, Row, Col, Accordion, Card, Button, ButtonGroup } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import { Table, Row, Col, Accordion, Card, Button, ButtonGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faHeadset, faFlagCheckered } from '@fortawesome/free-solid-svg-icons';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { faTrophy, faSort } from '@fortawesome/free-solid-svg-icons';
 import './PewPewCupComponentStyle.css';
 
 class PewPewCupComponent extends Component {
@@ -12,6 +13,7 @@ class PewPewCupComponent extends Component {
         };
 
         this.SeasonOneClick = this.SeasonOneClick.bind(this);
+        this.DiscordLink = this.DiscordLink.bind(this);
 
     }
 
@@ -24,13 +26,15 @@ class PewPewCupComponent extends Component {
         this.props.history.push(path);
     }
 
- 
+    DiscordLink() {
+        window.open("https://discord.gg/NSgGG3ta", "_blank")
+    }
 
     render() {
         return (
             <Col >
                 <Row>
-                   
+
                     <Col xl={11} className="Season2Title">
                         Season 2
                     </Col>
@@ -38,8 +42,94 @@ class PewPewCupComponent extends Component {
                         Season 1
                     </Col>
                 </Row>
-                <Row>
+                <Row className="pewpewDescriptionRow">
                     <Col>
+                        <img src="images/CloseArms041Uzi1Uzi.png" className="RobotImages" alt="Uzi" />
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Col className="pewpewTitle">
+                                PewPew European Cup
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="pewpewDescription">
+                                5v5 competitive Hyper Dash tournament
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Button onClick={this.DiscordLink} variant="outline-info">
+                                    <span className="joinDiscordButton">
+                                        Join Us On Discord
+                                    </span>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <img src="images/CloseArms140DefaultPistol0DefaultPistol.png" className="RobotImages" alt="Uzi" />
+                    </Col>
+                </Row>
+
+                <Row >
+                    <Col className="MatchTitle" >
+                        Ranking
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="matchStyleRow">
+                        <Row>
+                            <Col>
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Team</th>
+                                            <th>Points</th>
+                                            <th>Win</th>
+                                            <th>Losses</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+
+
+                                    </tbody>
+                                </Table>
+
+                            </Col>
+                        </Row>
+
+                    </Col>
+                </Row>
+                {/**
+                 * Ranking Players
+                 * 
+                 */}
+
+                <Row >
+                    <Col className="matchStyleAcordionRow playersRankingAccordion">
+                        Players
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="matchStyleRow">
+                        <Row>
+                            <Col>
+                                <BootstrapTable pagination search data={this.state.Players} striped={true} hover={true} bordered={true} >
+                                    <TableHeaderColumn dataField="Id" dataAlign="center" dataSort={true}>#<FontAwesomeIcon icon={faSort} className="sortIcon" /></TableHeaderColumn>
+                                    <TableHeaderColumn dataField="Tag" dataAlign="center" dataSort={true}>TAG<FontAwesomeIcon icon={faSort} className="sortIcon" /></TableHeaderColumn>
+                                    <TableHeaderColumn dataField="Name" tdStyle={{ whiteSpace: 'normal', wordWrap: 'break-word' }} isKey={true} dataAlign="center" dataSort={true}>NAME<FontAwesomeIcon icon={faSort} className="sortIcon" /></TableHeaderColumn>
+                                    <TableHeaderColumn dataField="Kills" dataAlign="center"  >KILLS</TableHeaderColumn>
+                                    <TableHeaderColumn dataField="Death" dataAlign="center"  >DEATHS</TableHeaderColumn>
+                                    <TableHeaderColumn dataField="KD" dataAlign="center" dataSort={true} >K/D<FontAwesomeIcon icon={faSort} className="sortIcon" /></TableHeaderColumn>
+                                </BootstrapTable>
+
+                            </Col>
+                        </Row>
+
                     </Col>
                 </Row>
 
